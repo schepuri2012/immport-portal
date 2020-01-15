6a4568c20a2f7b64a1b7653e0f80df38268bc32d
schepuri2012 <srinivaschepuri2012@gmail.com>
2020-01-15 17:56:52 -0500

    test

--- codedeploy/scripts/server_start.sh
+++ codedeploy/scripts/server_start.sh
@@ -6,10 +6,22 @@ cd /home/$APP_USER/apps/
 
 if [ -L ${APPLICATION_NAME} ] ; then
     if [ -e ${APPLICATION_NAME} ] ; then
-        /bin/bash ./$APPLICATION_NAME/bin/$APPLICATION_NAME status
-        /bin/bash ./$APPLICATION_NAME/bin/$APPLICATION_NAME stop
-        /bin/bash ./$APPLICATION_NAME/bin/$APPLICATION_NAME start    
+        {
+            set -e
+            /bin/bash ./$APPLICATION_NAME/bin/$APPLICATION_NAME status
+            /bin/bash ./$APPLICATION_NAME/bin/$APPLICATION_NAME start    
+        }
+    else 
+        error_exit "Symbolic link target file is missing!  Aborting."
     fi
+else 
+    error_exit "Application symbolic link is missing!  Aborting."
 fi
+
+error_exit()
+{
+	echo "$1" 1>&2
+	exit 1
+}
 
 exit 0
