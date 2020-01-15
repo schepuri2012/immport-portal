6aa4eb8bc8ae324782ae52f3adf2778d35d13682
schepuri2012 <srinivaschepuri2012@gmail.com>
2020-01-15 12:31:05 -0500

    test

--- codedeploy/scripts/afterInstall.sh
+++ codedeploy/scripts/afterInstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 APP_USER=ec2-user
-APP_NAME=immport-portal
-printenv
+
 echo "cd to the apps directory"
 cd /home/$APP_USER/apps/
 
@@ -14,5 +13,5 @@ if [ -f "$LATEST_ZIP" ]; then
     unzip $LATEST_ZIP
     chown -R ec2-user:ec2-user ${LATEST_ZIP%.*}
     echo "Creating symbolic link for ${LATEST_ZIP%.*}"
-    ln -s ${LATEST_ZIP%.*} $APP_NAME
+    ln -s ${LATEST_ZIP%.*} $APPLICATION_NAME
 fi
--- codedeploy/scripts/beforeInstall.sh
+++ codedeploy/scripts/beforeInstall.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 APP_USER=ec2-user
-APP_NAME=immport-portal
-printenv
+
 echo "cd to the apps directory"
 cd /home/$APP_USER/apps/
 
-if [ -L ${APP_NAME} ] ; then
-    if [ -e ${APP_NAME} ] ; then
-        echo "Reading the target file of the existing symbolic link"
-        target_file=`readlink -f $APP_NAME` 
+if [ -L ${APPLICATION_NAME} ] ; then
+    if [ -e ${APPLICATION_NAME} ] ; then
+        echo "Reading the target file of the current symbolic link"
+        target_file=`readlink -f $APPLICATION_NAME` 
         echo $target_file
-
 
         echo "if the applicatio is running"
         echo "Stop the application"
-        ./$APP_NAME/bin/$APP_NAME status
-        ./$APP_NAME/bin/$APP_NAME stop
+        ./$APPLICATION_NAME/bin/$APPLICATION_NAME status
+        ./$APPLICATION_NAME/bin/$APPLICATION_NAME stop
         
         echo "Back up previous application zip file"
         mv "${target_file}.zip" "${target_file}.zip.bkp"
 
         echo "Back up previous application folder"
         mv "${target_file}" "${target_file}.bkp" 
+
+        echo "Back up previous git.properties"
+        mv git.properties git.properties.bkp" 
     fi
 fi
 
-
-if [ -L ${APP_NAME} ] ; then
+if [ -L ${APPLICATION_NAME} ] ; then
         echo "Removing the existing symbolic link"
-        rm ${APP_NAME}
+        rm ${APPLICATION_NAME}
 fi
